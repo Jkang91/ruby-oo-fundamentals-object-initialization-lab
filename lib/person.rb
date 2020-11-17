e9e3fa56a0fdfa6f5eeb43e00d933ecb448db4a9
June Kang <Jkang34@gmail.com>
2020-11-16 20:45:22 -0500

    object initialization lab

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Dog 
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed 
+    end 
+    def name=(name_breed)
+        name, breed = name_breed.split 
+        @name = name 
+        @breed = breed 
+    end 
+    def name 
+        @name
+        @breed 
+    end 
+end 
+
